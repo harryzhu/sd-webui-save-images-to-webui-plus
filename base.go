ac00fead50cee0a9a1bd7c70bd4f4dcc165d134b
harryzhu <harry.chuh@gmail.com>
2022-08-31 23:27:07 +0800

    -

--- base.go
+++ base.go
@@ -2,37 +2,37 @@ package functions
 
 import (
 	"fmt"
-	"os"
-	"time"
+	// "os"
+	// "time"
 )
 
 func Hello() {
 	fmt.Println("Hello")
 }
 
-func GetEnv(s string, vDefault string) string {
-	v := os.Getenv(s)
-	if v == "" {
-		return vDefault
-	}
-	return v
-}
+// func GetEnv(s string, vDefault string) string {
+// 	v := os.Getenv(s)
+// 	if v == "" {
+// 		return vDefault
+// 	}
+// 	return v
+// }
 
-func GetNowUnix() int64 {
-	return time.Now().Unix()
-}
+// func GetNowUnix() int64 {
+// 	return time.Now().Unix()
+// }
 
-func SliceUnique(lines []string) (linesUnique []string) {
-	unique := make(map[string]string, len(lines))
+// func SliceUnique(lines []string) (linesUnique []string) {
+// 	unique := make(map[string]string, len(lines))
 
-	for _, line := range lines {
-		unique[MD5(line)] = line
-	}
+// 	for _, line := range lines {
+// 		unique[MD5(line)] = line
+// 	}
 
-	for _, v := range unique {
-		if v != "" {
-			linesUnique = append(linesUnique, v)
-		}
-	}
-	return linesUnique
-}
+// 	for _, v := range unique {
+// 		if v != "" {
+// 			linesUnique = append(linesUnique, v)
+// 		}
+// 	}
+// 	return linesUnique
+// }
