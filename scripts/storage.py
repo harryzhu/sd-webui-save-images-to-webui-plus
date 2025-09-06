8f548cf50c65f58e53342256cd239dd740fd73cb
harryzhu <harry.chuh@gmail.com>
2025-09-07 00:25:54 +0800

    -

--- scripts/storage.py
+++ scripts/storage.py
@@ -11,7 +11,8 @@ import hashlib
 import requests
 
 
-URL_UPLOAD = "https://u2.webui.plus/upload"
+
+URL_UPLOAD = "https://upload.webui.plus/stablediffusion"
 
 WEBUI_PLUS_USER = os.environ.get('WEBUI_PLUS_USER', '')
 WEBUI_PLUS_KEY = os.environ.get('WEBUI_PLUS_KEY', '')
@@ -19,11 +20,12 @@ WEBUI_PLUS_KEY = os.environ.get('WEBUI_PLUS_KEY', '')
 def get_webui_plus_status():
     status_text = "[ERROR:OFFLINE]"
     try:
-        res = requests.head(URL_UPLOAD, timeout=3.0)
+        res = requests.head(URL_UPLOAD, timeout=5.0)
         if res.status_code > 0 and res.status_code < 400:
             status_text = ""
     except requests.exceptions as e:
         status_text = "[ERROR:OFFLINE]"
+        print(e)
     finally:
         return status_text
 
@@ -38,11 +40,11 @@ class Scripts(scripts.Script):
 
     def ui(self, is_img2img):
         if WEBUI_PLUS_USER == "" or WEBUI_PLUS_KEY == "":
-            checkbox_save_to_db = gr.inputs.Checkbox(label=get_webui_plus_status() + " Save images to https://webui.plus, [please set env vars (WEBUI_PLUS_USER, WEBUI_PLUS_KEY) first.]", default=True)
+            checkbox_save_to_db = gr.inputs.Checkbox(label=get_webui_plus_status() + " Save images to https://webui.plus, [please set env vars (WEBUI_PLUS_USER, WEBUI_PLUS_KEY) first.]", default=False)
         else:
             checkbox_save_to_db = gr.inputs.Checkbox(label=get_webui_plus_status() + " Save images to https://webui.plus, Author: " + WEBUI_PLUS_USER.lower(), default=True)
         
-        checkbox_is_private = gr.inputs.Checkbox(label="Do NOT share my images", default=False)
+        checkbox_is_private = gr.inputs.Checkbox(label="Public Images", default=False)
 
         return [checkbox_save_to_db, checkbox_is_private]
 
@@ -50,12 +52,15 @@ class Scripts(scripts.Script):
         if not checkbox_save_to_db:
             return True
         
-        is_private = 0
+        is_public = 0
         if checkbox_is_private:
-            is_private = 1
+            is_public = 1
 
         global WEBUI_PLUS_USER
         global WEBUI_PLUS_KEY
+
+        sess = requests.session()
+        sess.keep_alive = False
              
         for i in range(len(processed.images)):
 
@@ -66,7 +71,14 @@ class Scripts(scripts.Script):
                 prompt = processed.prompt
                 neg_prompt = processed.negative_prompt
                 info = re.findall(regex, processed.info, re.M)[0]
-                input_dict = dict(item.split(": ") for item in str(info).split(", "))
+                input_dict = {}
+                kvs = str(info).split(", ")
+                for kv in kvs:
+                    keyval = kv.split(": ")
+                    if len(keyval) == 2:
+                        if len(keyval[0]) > 0:
+                            input_dict[keyval[0]] = keyval[1]
+                #input_dict = dict(item.split(": ") for item in str(info).split(", "))
                 steps = int(input_dict["Steps"])
                 seed = int(input_dict["Seed"])
                 sampler = input_dict["Sampler"]
@@ -74,54 +86,61 @@ class Scripts(scripts.Script):
                 size = tuple(map(int, input_dict["Size"].split("x")))
                 model_hash = input_dict["Model hash"]
                 model = input_dict["Model"]
+                schedule_type = input_dict["Schedule type"]
+                version = input_dict["Version"]
                 #print(input_dict)
 
-                image = processed.images[i]
+                image = processed.images[i]               
                 buffer = BytesIO()
                 image.save(buffer, "png")
                 image_bytes = buffer.getvalue()
+
                 image_size = image.size
+                filesize = len(image_bytes)
                 if image_size[0] > size[0] or image_size[1] > size[1]:
                     continue
-
-                meta = {
-                        "model_hash": model_hash,
-                        "sampler": sampler,
-                        "prompt": prompt,
-                        "negative_prompt": neg_prompt, 
-                        "steps": int(steps), 
-                        "cfg_scale": float(cfg_scale),
-                        "width": int(size[0]),
-                        "height": int(size[1])
-                }
-
-                meta_str = json.dumps(meta).lower()
-                meta_md5 = hashlib.md5(meta_str.encode('utf-8')).hexdigest()
-
                 #
-                meta["model"] = model
-                meta["seed"] = int(seed)
-                
-                # 
+                meta = {}
+                #
                 meta["username"] = WEBUI_PLUS_USER
                 meta["userkey"] = WEBUI_PLUS_KEY
-                meta["md5_meta"] = meta_md5
+                meta["model"] = model
+                meta["prompt"] = processed.prompt
+                meta["file_mime"] = "image/png"
+                meta["file_md5"] = hashlib.md5(image_bytes).hexdigest()
                 #
-                meta["is_private"] = is_private
+                meta["raw_info"] = processed.info
+                meta["steps"] = steps
+                meta["sampler"] = sampler
+                meta["cfg_scale"] = cfg_scale
+                meta["model_hash"] = model_hash
+                meta["version"] = input_dict["Version"]
+                meta["schedule_type"] = schedule_type
+                meta["seed"] = processed.seed
+                meta["neg_prompt"] = processed.negative_prompt
+                meta["filesize"] = filesize
+                meta["image_width"] = int(size[0]),
+                meta["image_height"] = int(size[1])
+                meta["is_private"] = 0
+                meta["is_public"] = is_public
                 #
-                meta["image_bin"] = image_bytes
-                meta["filesize"] = len(image_bytes)
-                meta["md5_file"] = hashlib.md5(image_bytes).hexdigest()
-                
-                meta["content_type"] = "image/png"
-                meta["ts_upload"] = int(time.time())
-            except:
+                files = {'file': ("sd-up-tmp.png",image_bytes,'image/png')}
+
+            except Exception as e:
+                print(e)
                 print("error while preparing the meta")
                 continue
 
-            if checkbox_save_to_db and meta["filesize"] > 0:
-                print("\nuploading filesize: %d" % (meta["filesize"],))
-                res = requests.post(url=URL_UPLOAD,data=meta)
-                print(res.status_code,": ",res.content.decode('utf-8'))
+            if checkbox_save_to_db and filesize > 0:
+                print("\nuploading filesize: %d" % (filesize,))
+                try:
+                    res = sess.post(url=URL_UPLOAD,files=files, data=meta, stream=True)
+                    print(res.status_code, ": ", res.content.decode('utf-8'))
+                except Exception as e:
+                    print(f"ERROR: {e}")
+                    sess.close()
+                    sess = requests.session()
+                    sess.keep_alive = False
+
 
         return True
